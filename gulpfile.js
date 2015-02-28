d26a3b1f01235d4e56c6981fff3460c21d74e329
lagonnebula <laginnebula2@gmail.com>
2015-02-28 16:21:49 +0100

    Architecture

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
+gulp.task('dist', function(){
+	//Building app
+});
